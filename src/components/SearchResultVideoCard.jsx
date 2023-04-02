b4463fb8a432e2d34b7ee056b7176f7152ff6adc
syprogrammer <sonaly647@gmail.com>
2023-04-02 23:44:14 +0530

    sidenav issue on deployment

--- src/components/SearchResultVideoCard.jsx
+++ src/components/SearchResultVideoCard.jsx
@@ -19,7 +19,7 @@ const SearchResultVideoCard = ({ video }) => {
                     )}
                 </div>
                 <div className="flex flex-col ml-4 md:ml-6 mt-4 md:mt-0 overflow-hidden">
-                    <span className="text-lg md:text-2xl font-semibold line-clamp-2 text-white text-left">
+                    <span className="text-sm md:text-2xl font-semibold line-clamp-2 text-white text-left">
                         {video?.title}
                     </span>
                     <span className="empty:hidden text-sm line-clamp-1 md:line-clamp-2 text-white/[0.7] text-left md:pr-24 md:my-4">
