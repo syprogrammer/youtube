c923f92f4a2485789977282b1d69ee6340a7ef91
syprogrammer <sonaly647@gmail.com>
2023-04-03 00:15:20 +0530

    sidenav issue in mobile device fixed

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -57,8 +57,8 @@ const LeftNav = () => {
 
     return (
         <div
-            className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
-                mobileMenu ? "translate-x-5" : ""
+            className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10  md:translate-x-0 transition-all ${
+                mobileMenu ? "translate-x-2" : "translate-x-[-240px]"
             }`}
         >
             <div className="flex px-5 flex-col">
